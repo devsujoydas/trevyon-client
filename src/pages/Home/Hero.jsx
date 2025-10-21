95ae41aae693d6246735830f1f8a295de153ecc6
devsujoydas <devsujoydas@gmail.com>
2025-10-21 23:25:03 +0600

    update

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -15,7 +15,7 @@ const heroText = (img) => (
   <div className=' text-white bg-no-repeat bg-right bg-cover' style={{ backgroundImage: `url('${img}')` }} >
     <div className='md:h-[900px] h-[450px] bg-[#0e00003f]  w-full  flex justify-center  items-center'>
       <div className='md:h-[900px] w-full flex justify-center  items-center'>
-        <div className='max-w-screen-2xl w-full xl:mx-auto md:mx-10 mx-5 py-20 space-y-5 md:space-y-10'>
+        <div className=' w-main w-full py-20 space-y-5 md:space-y-10'>
           <h1 className='font-semibold md:text-6xl text-3xl'>Discover your skin's true <br className='md:block hidden' /> potential</h1>
           <p className=' text-sm md:text-xl'>Premium skincare that combines innovation with clean, effective ingredients for all skin types.</p>
           <div className=' text-sm md:text-lg flex gap-5'>
